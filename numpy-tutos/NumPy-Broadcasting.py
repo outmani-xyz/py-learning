11e02ecff4631a9b5c39c40c40aeaa31e0c90e2c
outmani.me <tilmido93@gmail.com>
2026-03-31 14:16:31 +0100

    broadcasting numpy"

---
+++ numpy-tutos/NumPy-Broadcasting.py
@@ -0,0 +1,15 @@
+import numpy as np 
+
+arr_2d = np.array([
+    [2, 4, 5],
+    [12, 14, 15],
+    [23, 44, 65],
+    [23, 44, 65],
+])
+
+arr_1d = np.array(
+    [2 , 44,44])
+
+
+print(arr_2d + arr_1d)
+
